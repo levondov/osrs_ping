1c2f572324dd4526345fa5b875a214e793b7f564
Levon Dovlatyan <levondov@berkeley.edu>
2016-07-05 23:51:20 +0000

    minor changes

--- world_pinger.py
+++ world_pinger.py
@@ -1,6 +1,5 @@
 import subprocess
 import numpy as np
-import matplotlib.pyplot as plt
 
 worlds = np.linspace(1,94,94)
 exceptions = np.array([7, 15, 23, 24, 31, 32, 39, 40, 47, 48, 55, 
@@ -11,3 +10,5 @@ def ping_world(world):
     address = "oldschool" + str(world) + ".runescape.com"
     ping_resp = subprocess.Popen(["/bin/ping", "-c1", "-w100", address], stdout=subprocess.PIPE).stdout.read()
     return ping_resp
+
+print ping_world(53)
